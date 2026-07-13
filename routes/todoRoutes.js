eb32266d03e329b5a4f03138855f8f9922bd6031
saranya <saranyabhaskar2006@gmail.com>
2026-07-13 15:16:22 +0530

    Refactor todo routes to use lowercase model name

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Todo = require("../models/Todo");
+const Todo = require("../models/todo");
 
 router.get("/", async (req, res) => {
   try {
