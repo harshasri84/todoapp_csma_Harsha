707d44a88414edc04702823e113a95b38c8eb669
saranya <saranyabhaskar2006@gmail.com>
2026-07-14 11:50:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dns =require("dns")
-dns.setServers(["8.8.8.8","8.8.4.4"])
+// const dns =require("dns")
+// dns.setServers(["8.8.8.8","8.8.4.4"])
 const cors = require("cors");
 require("dotenv").config();
 
